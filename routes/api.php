c9ace8d42f86f860a12f54b9935a7563a9fe6bdf
Rodrigo Denner <rodrigodenner.art@gmail.com>
2025-05-12 14:48:27 -0300

    fix: refactor method store

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   return response()->json([
-    'message' => 'BEM VINDO A API Hotel Taiyo'
+    'message' => 'BEM VINDO A API La Suite '
   ]);
 });
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   return response()->json([
-    'message' => 'BEM VINDO A API Hotel Taiyo'
+    'message' => 'BEM VINDO A API La Suite '
   ]);
 });
